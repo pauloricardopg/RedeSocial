6edb7d0778a2edd65b03524c5db7b05c1920cc86
Paulo Ricardo Pereira Gomes <96184752+pauloricardopg@users.noreply.github.com>
2025-03-27 21:51:35 -0300

    __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -8,9 +8,13 @@ app = Flask(__name__)
 
 
 app.config['SECRET_KEY'] = '46ca2defb01cc7f0b122f36e6ca22e9d'
-
 base_dir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "comunidade.db")}'
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(base_dir, "comunidade.db")}'
+
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
